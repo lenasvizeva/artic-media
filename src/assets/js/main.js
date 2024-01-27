424e4308a60c54de446f2dfd95fedd4f04908f82
elena <kaiout@mail.ru>
2024-01-28 00:49:25 +0400

    animations

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,11 +2,14 @@
 
 const toggleMenu = () => {
     let menu = document.getElementById('menu')
+    let body = document.getElementById('body')
 
     if (menu.classList.contains('active')) {
         menu.classList.remove('active')
+        body.classList.remove('hidden')
     } else {
         menu.classList.add('active')
+        body.classList.add('hidden')
     }
 }
 
