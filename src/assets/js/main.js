f21160e895f9022df09d61ea2c6d4f2f84e3e21b
elena <kaiout@mail.ru>
2024-01-29 00:36:12 +0400

    modal

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,31 @@
+const modal = document.getElementById('modal')
+const body = document.getElementById('body')
+const form = document.getElementById('callback-form')
+const finishForm = document.getElementById('finish-form')
+
+
+const toggleModal = () => {
+    if (modal.classList.contains('open')) {
+        modal.classList.remove('open')
+        body.classList.remove('hidden')
+
+        form.classList.add('active') 
+        finishForm.classList.remove('active')
+    } else {
+        modal.classList.add('open')
+        body.classList.add('hidden')
+    }
+}
+
+const handleFormSubmit = () => {
+    event.preventDefault();
+
+    if (form.classList.contains('active') ) {
+        form.classList.remove('active')    
+        finishForm.classList.add('active')
+    }     
+}
+
 const toggleMenu = () => {
     let menu = document.getElementById('menu')
     let body = document.getElementById('body')
